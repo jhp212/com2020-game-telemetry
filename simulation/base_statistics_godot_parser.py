57fd8263353956336470c70f51b9d4abf340f49e
Ollie Robson <ojr209@exeter.ac.uk>
2026-02-03 12:54:29 +0000

    Created a python script which imports the tower data and enemy data into python

---
+++ simulation/base_statistics_godot_parser.py
@@ -0,0 +1,37 @@
+with open("game/Godot/Singleton/game_data.gd") as game_data:
+    data = list(map(lambda line: line.removesuffix("\n"), game_data.readlines()))
+    game_data.close()
+
+tower_data_start: int = data.index("var tower_data = {")
+potentialtowerdict = ''.join(map(lambda line: line.lstrip("\t"),data[tower_data_start+1:]))
+stack = ["{"]
+for index,char in enumerate(potentialtowerdict):
+    if char in ['{', '[', '(','"']:
+        if char == '"' and stack[-1] == '"':
+            stack.pop()
+        else:
+            stack.append(char)
+    elif char in ['}',']',')','"']:
+        if  ['{', '[', '(','"'].index(stack[-1]) == ['}',']',')','"'].index(char):
+            stack.pop()
+    if len(stack) == 0:
+        tower_data = eval("{"+potentialtowerdict[:index+1])
+        break
+print(tower_data)
+
+enemy_data_start: int = data.index("var enemy_data = {")
+potentialenemydict = ''.join(map(lambda line: line.lstrip("\t"),data[enemy_data_start+1:]))
+stack = ["{"]
+for index,char in enumerate(potentialenemydict):
+    if char in ['{', '[', '(','"']:
+        if char == '"' and stack[-1] == '"':
+            stack.pop()
+        else:
+            stack.append(char)
+    elif char in ['}',']',')','"']:
+        if  ['{', '[', '(','"'].index(stack[-1]) == ['}',']',')','"'].index(char):
+            stack.pop()
+    if len(stack) == 0:
+        enemy_data = eval("{"+potentialenemydict[:index+1])
+        break
+print(enemy_data)
