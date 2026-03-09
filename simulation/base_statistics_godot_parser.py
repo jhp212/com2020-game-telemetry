c5cb8564cf1dd3292a6e7ba1ef04d75cea923f82
Doruk <doruk.aksu@gmail.com>
2026-03-09 22:27:21 +0000

    Revert "Merge branch 'sprint_2' into admin-requests-development"
    This reverts commit 3fe3754bca0541c117f4403973f7848314f89f78, reversing
    changes made to 8a805c47955979d94c4a0347df046831af7d971d.

--- simulation/base_statistics_godot_parser.py
+++ simulation/base_statistics_godot_parser.py
@@ -2,14 +2,14 @@ with open("game/Godot/Singleton/game_data.gd") as game_data:
     data = list(map(lambda line: line.removesuffix("\n"), game_data.readlines()))
     game_data.close()
 
-for item in data[data.index("func reset():"):]:
-    if item[0:15] == '\tbase_health = ':
+for item in data:
+    if item[0:18] == 'var base_health = ':
         healthind = data.index(item)
-    elif item[0:8] == '\tmoney =':
+    elif item[0:12] == 'var money :=':
         moneyind = data.index(item)
 
-player_base_health = eval(data[healthind].lstrip('\tbase_health = '))
-player_base_money = eval(data[moneyind].lstrip('\tmoney ='))
+player_base_health = eval(data[healthind].lstrip('var base_health = '))
+player_base_money = eval(data[moneyind].lstrip('var money :='))
 
 tower_data_start: int = data.index("var tower_data = {")
 potentialtowerdict = ''.join(map(lambda line: line.lstrip("\t"),data[tower_data_start+1:]))
