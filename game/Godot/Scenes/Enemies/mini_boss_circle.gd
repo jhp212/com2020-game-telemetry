82dd601f8327cbb56f9332d6bb2857ec0a48fb15
dd524-exeter <dd524@exeter.ac.uk>
2026-03-12 18:28:44 +0000

    Created damage visuals to enemies and death animation

--- game/Godot/Scenes/Enemies/mini_boss_circle.gd
+++ game/Godot/Scenes/Enemies/mini_boss_circle.gd
@@ -1,15 +1,17 @@
 extends PathFollow2D
 
 # Load enemy stats from GameData
-var speed = GameData.enemy_data["small_circle"]["speed"]
-var health =  GameData.enemy_data["small_circle"]["health"]
-var damage =  GameData.enemy_data["small_circle"]["damage"]
+var speed = GameData.enemy_data["mini_boss_circle"]["speed"]
+var health =  GameData.enemy_data["mini_boss_circle"]["health"]
+var damage =  GameData.enemy_data["mini_boss_circle"]["damage"]
 
 # Signals for when the enemy deals damage or dies
 signal enemy_damage(damage)
 signal enemy_death()
 
-
+func _ready() -> void:
+	Telemetry.log_event("boss_start",  {"enemy_id": 4})
+	print("boss has started")
 
 func on_hit(damage):
 	# Reduce health when hit
@@ -22,26 +24,23 @@ func die(condition):
 	# condition 1 = reached the end of the path
 	if condition == 0:
 		# Give money to player and log telemetry event
-		GameData.add_money(GameData.enemy_data["small_circle"]["cash"])
-		Telemetry.log_event("enemy_defeated", {"enemy_id": 1})
+		GameData.add_money(GameData.enemy_data["mini_boss_circle"]["cash"])
+		Telemetry.log_event("boss_defeated", {"enemy_id": 4})
+	if condition == 1:
+		# Emit death signal and log telemetry event, then remove enemy
+		Telemetry.log_event("boss_fail", {"enemy_id": 4})
 	$Body/AnimatedSprite2D.play("death")
 	await $Body/AnimatedSprite2D.animation_finished
 	enemy_death.emit()
 	self.queue_free()
 
 func _physics_process(delta: float) -> void:
-	if health < GameData.enemy_data["small_circle"]["health"] * 0.75 and health >= GameData.enemy_data["small_circle"]["health"] * 0.5:
-		$Body/AnimatedSprite2D.play("75%health")
-	elif health < GameData.enemy_data["small_circle"]["health"] * 0.5 and health >= GameData.enemy_data["small_circle"]["health"] * 0.25:
-		$Body/AnimatedSprite2D.play("50%health")
-	elif health < GameData.enemy_data["small_circle"]["health"] * 0.25 and health > 0:
-		$Body/AnimatedSprite2D.play("25%health")
-	
 	if progress_ratio == 1.0:
 		# Do damage to the player if enemy reaches the end of the path and die
 		enemy_damage.emit(damage)
 		die(1)
 	move(delta)
+
 
 func move(delta):
 	# Move along the path
